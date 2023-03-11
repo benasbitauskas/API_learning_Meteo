bc6da0ebb820b878b6abefeeb0ed41abb027c407
Benas Bitauskas <benasbitauskas@gmail.com>
2023-03-11 20:48:51 +0200

    places

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import requests
 from pprint import pprint
+from datetime import date
+
+today = str(date.today())
+station_code = input('Įveskite stoties kodą: ')
+base_url = 'https://api.meteo.lt/v1/'
+stations = base_url+'stations/'
+weather_info = stations+station_code+'/observations/'+today
+places = base_url+'places/'
+
+places = requests.get(places).json()
+
+pprint(places)
 
 
-base_url = 'https://api.meteo.lt/v1/stations/'
-
-
-stations = requests.get(base_url).json()
-
-pprint(stations)
