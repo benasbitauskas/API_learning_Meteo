6cf439abf9a92b7f15aa10a0cd152cb793512586
Benas Bitauskas <benasbitauskas@gmail.com>
2023-03-12 19:44:43 +0200

    working with extrackting data from json format

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import requests
 from pprint import pprint
 from datetime import date
 
+# city = input("Įveskite miestą: ")
 base_url = 'https://api.meteo.lt/v1/'
 today = str(date.today())
 stations = base_url + 'stations/'
-# weather_info = stations+station_code+'/observations/'+today
-places = base_url + 'places/'
+forecast = base_url + 'places/' + 'Kaunas' + '/forecasts/long-term'
+forecast = requests.get(forecast).json()['airTemperature']
 
-places = requests.get(places).json()
-
-pprint(places)
+# for key in list(forecast.keys()):
+#     if key == 'airTemperature':
+pprint(forecast)
